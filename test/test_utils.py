545a855201e6968c764ba5805a0f6fd4e4642650
Magnun Leno <magnun.leno@gmail.com>
2015-01-09 19:34:40 -0200

    Some test for slugify

---
+++ test/test_utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from folios import utils
+
+
+def test_slugify():
+    assert utils.slugify('Testing slugs!') == 'testing-slugs'
+    assert utils.slugify('Açáéóíãà/-123') == 'acaeoiaa-123'
+    assert utils.slugify('nothing-¢£¤¥§©in-between') == 'nothing-in-between'
