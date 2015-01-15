f4bff2e654326d8830717e7369f4de7938e74abe
Magnun Leno <magnun.leno@gmail.com>
2015-01-15 00:27:25 -0200

    Glues everything together

--- folios/__init__.py
+++ folios/__init__.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-__all__ = ['utils']
+import os
+import sys
+from folios import utils
+from folios import cmd
+from folios import exceptions
 
-from . import utils
+__ROOT__ = os.path.abspath(os.path.dirname(__file__))
+
+__all__ = ['utils', 'cmd', 'exceptions']
+__version__ = '0.1.0'
+
+
+def main():
+    try:
+        cmd.main(sys.argv[1:])
+    except exceptions.FoliosBaseException as e:
+        print(e.message)
+        exit(1)
+    else:
+        exit(0)
