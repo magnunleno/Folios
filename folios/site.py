100171cdd1820d3ca545c6cd99ce0be4c1fd78ef
Magnun Leno <magnun.leno@gmail.com>
2015-01-15 20:02:11 -0200

    First site.py code

---
+++ folios/site.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import os
+
+from folios.exceptions import FoliosAbortException
+from folios.utils import joinPath
+
+
+class Site(object):
+    __slots__ = (
+        'basepath',
+        'settings',
+        )
+
+    def __init__(self):
+        self.basepath = os.path.abspath(os.getcwd())
+        if not self.__isFoliosSite(self.basepath):
+            self.basepath = self.__resolveRootFolder(self.basepath)
+
+    def __resolveRootFolder(self, folder):
+        if not os.path.basename(folder):
+            errstr = "I'm sorry Dave. I'm afraid I can't do that.\n" +\
+                "Are you sure you're inside a Folios site folder?"
+            raise FoliosAbortException(errstr)
+
+        if self.__isFoliosSite(folder):
+            return folder
+
+        new_folder = os.path.abspath(joinPath(folder, os.path.pardir))
+        self.__resolveRootFolder(new_folder)
+
+    def __isFoliosSite(self, path):
+        return os.path.exists(joinPath(path, '.folios'))
