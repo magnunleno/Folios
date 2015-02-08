16e046303f138e175caade84d7d337fac57888c3
Magnun Leno <magnun.leno@gmail.com>
2015-02-08 02:34:11 -0200

    Fix test to use new name

--- folios/tests/test_cmd.py
+++ folios/tests/test_cmd.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-from folios import cmd
+from folios import cli
 from nose.tools import raises
 from docopt import DocoptExit
 
 
 @raises(DocoptExit)
 def test_main_error():
-    cmd.main([])
+    cli.main([])
 
 
 @raises(DocoptExit)
 def test_main_not_implemented():
-    cmd.main(['NotImplemented'])
+    cli.main(['NotImplemented'])
 
 
 @raises(DocoptExit)
 def test_main_init_error():
-    cmd.main(['-v'])
+    cli.main(['-v'])
