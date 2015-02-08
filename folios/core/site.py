94226d9bb544b92643eaebaac5dda3786722c430
Magnun Leno <magnun.leno@gmail.com>
2015-02-08 02:37:27 -0200

    Refactor site core

--- folios/core/site.py
+++ folios/core/site.py
@@ -2,33 +2,48 @@
 # encoding: utf-8
 
 import os
+import logging
 
-from folios.exceptions import FoliosAbortException
-from folios.utils import joinPath
+from folios.core import logger
+from folios.core import __rootfolder__
+from folios.core.exceptions import FoliosAbortException
+from folios.core.settings import Settings
 
 
 class Site(object):
     __slots__ = (
         'basepath',
         'settings',
+        'log',
         )
 
-    def __init__(self):
-        self.basepath = os.path.abspath(os.getcwd())
+    def __init__(self, basepath, settings):
+        # Saves basepath
+        self.basepath = os.path.abspath(basepath)
         if not self.__isFoliosSite(self.basepath):
-            self.basepath = self.__resolveRootFolder(self.basepath)
+            errstr = "I'm sorry Dave. I'm afraid I can't do that.\n" +\
+                "Are you sure you're inside a Folios site folder?"
+            raise FoliosAbortException(errstr)
 
-    def __resolveRootFolder(self, folder):
+        # Initialize settings and log
+        self.settings = settings
+        self.log = logger.get_logger('site', self.settings)
+        self.log.debug("Basepath: '{}'".format(self.basepath))
+
+    @classmethod
+    def resolveRootFolder(kls, folder):
         if not os.path.basename(folder):
             errstr = "I'm sorry Dave. I'm afraid I can't do that.\n" +\
                 "Are you sure you're inside a Folios site folder?"
             raise FoliosAbortException(errstr)
 
-        if self.__isFoliosSite(folder):
+        if kls.__isFoliosSite(folder):
             return folder
 
-        new_folder = os.path.abspath(joinPath(folder, os.path.pardir))
-        self.__resolveRootFolder(new_folder)
+        # Go back one level and try again
+        new_folder = os.path.abspath(os.path.join(folder, os.path.pardir))
+        kls.resolveRootFolder(new_folder)
 
-    def __isFoliosSite(self, path):
-        return os.path.exists(joinPath(path, '.folios'))
+    @classmethod
+    def __isFoliosSite(kls, path):
+        return os.path.exists(os.path.join(path, __rootfolder__))
