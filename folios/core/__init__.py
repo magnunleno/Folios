1ee875a0ca20c2378a98e2116d9098d9943c6c61
Magnun Leno <magnun.leno@gmail.com>
2015-02-08 02:32:35 -0200

    Cleans core.__init__

--- folios/core/__init__.py
+++ folios/core/__init__.py
@@ -3,21 +3,6 @@
 
 import os
 import sys
-from folios import utils
-from folios import cmd
-from folios import exceptions
 
-__ROOT__ = os.path.abspath(os.path.dirname(__file__))
-
-__all__ = ['utils', 'cmd', 'exceptions']
-__version__ = '0.1.0'
-
-
-def main():
-    try:
-        cmd.main(sys.argv[1:])
-    except exceptions.FoliosBaseException as e:
-        print(e.message)
-        exit(1)
-    else:
-        exit(0)
+__all__ = ['exceptions', 'logger', 'settings', 'site', 'utils']
+__rootfolder__ = '.folios'
