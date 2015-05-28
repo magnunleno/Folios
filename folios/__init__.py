4e9c2d94354f10a1341d4de814289589416b41cb
Magnun Leno <magnun.leno@gmail.com>
2015-05-28 00:55:01 -0300

    Adds license to files

--- folios/__init__.py
+++ folios/__init__.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python3
 # encoding: utf-8
+#
+# Folios - Yet Another Static Site Generator
+# Copyright (C) 2015 - Magnun Leno
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Tomorrow will take us away
+Far from home
+No one will ever know our names
+But the bard's songs will remain
+Tomorrow all will be known
+And you're not alone
+So don't be afraid
+In the dark and cold
+'Cause the bard's songs will remain
+                    --- The Bard's Song (Blind Guardian)
+"""
 
 import os
 
